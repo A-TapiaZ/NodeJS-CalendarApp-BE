5ebf58df29f558484a1b718c0decdf5ac83a215d
Alejo Tapias <alejotapias0414@gmail.com>
2020-09-07 16:36:52 -0500

    CalendarApp v1.0

---
+++ public/precache-manifest.1eacb44f866e85c61348145324c51a15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ea0208e28a2b440f409a4970cb44be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09fc8a6a912764d43534",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "a7776aa6b406362aa17f",
+    "url": "/static/css/main.95f0e568.chunk.css"
+  },
+  {
+    "revision": "09fc8a6a912764d43534",
+    "url": "/static/js/2.5aac843c.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.5aac843c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7776aa6b406362aa17f",
+    "url": "/static/js/main.b0c7fdec.chunk.js"
+  },
+  {
+    "revision": "afbe67ac097fa01e74de",
+    "url": "/static/js/runtime-main.155d3ec1.js"
+  }
+]);
